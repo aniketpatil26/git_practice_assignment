296da23accbf8f2a4ed16d3e25d0c6c429700c28
anusha <anushaelluri30@gmail.com>
2022-12-10 22:21:00 +0530

    your code is done aniket please check

--- prime.js
+++ prime.js
@@ -1,11 +1,1 @@
-// prime number;
-
-let num1=true;
-if(num>1){
-   for(i=2;i<num;i++){
-       if(num%i===0){
-          num1=false;
-          break;
-       }
-   }
-}
+// prime number;let num1=true;if(num>1){   for(i=2;i<num;i++){       if(num%i===0){          num1=false;          break;       }   }}if (num1){      console.log("Yes");}else{     console.log("No");}
